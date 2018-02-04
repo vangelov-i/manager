e89fede7c79dbf2355a324c40a5853caf6071f1e
Iliyan Vangelov <iliyan.vangelov@gmail.com>
2018-02-04 18:19:25 +0200

    Introduce redux flow for handling user login - Add fail and success actions and reducers - Introduce logic handling error messaging - Add spinner to login form - Use ReduxThunk for async dispatching of actions

---
+++ src/components/LoginForm.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { Card, CardSection, Input, Button, Spinner } from './common';
+import { emailChanged, passwordChanged, loginUser } from '../actions';
+
+class LoginForm extends Component {
+  onEmailChange(text) {
+    this.props.emailChanged(text);
+  }
+
+  onPasswordChange(text) {
+    this.props.passwordChanged(text);
+  }
+
+  onButtonPress() {
+    const { email, password } = this.props;
+
+    this.props.loginUser({ email, password });
+  }
+
+  renderError() {
+    if (this.props.error) {
+      return (
+        <View style={{ backgroundColor: 'white' }}>
+          <Text style={styles.errorTextStyle}>
+            {this.props.error}
+          </Text>
+        </View>
+      );
+    }
+  }
+
+  renderButton() {
+    if (this.props.loading) {
+      return <Spinner size='large' />;
+    }
+
+    return (
+      <Button onPress={() => this.onButtonPress()}>
+        Log in
+      </Button>
+    );
+  }
+
+  render() {
+    return (
+      <Card>
+        <CardSection>
+          <Input
+            label="Email"
+            placeholder="email@gmail.com"
+            onChangeText={text => this.onEmailChange(text)}
+            value={this.props.email}
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input
+            secureTextEntry
+            label="Password"
+            placeholder="password"
+            onChangeText={text => this.onPasswordChange(text)}
+            value={this.props.password}
+          />
+        </CardSection>
+
+        {this.renderError()}
+
+        <CardSection>
+          {this.renderButton()}
+        </CardSection>
+      </Card>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  const { email, password, error, loading } = state.auth;
+
+  return { email, password, error, loading };
+};
+
+const actions = {
+  emailChanged,
+  passwordChanged,
+  loginUser
+};
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+};
+
+export default connect(mapStateToProps, actions)(LoginForm);
